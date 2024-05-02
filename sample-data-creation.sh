1827dd65b57c2c34e2693712f6e2a73bf816520c
Leon Matthews <leon@digitaladvisor.nz>
2024-05-03 11:03:35 +1200

    Create bash script to populate test folder

---
+++ sample-data-creation.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/bash
+set -o errexit
+set -o nounset
+
+###################################################
+# Create subset of IMDB data for testing purposes #
+###################################################
+
+##########
+# Config #
+##########
+NUM_ROWS=100000
+OUTPUT_FOLDER="tests/data/"
+
+
+# Number of arguments
+if [ $# -eq 0 ]
+then
+    echo "Provide folder containing IMDB data files"
+    echo "usage: $0 DATA_FOLDER"
+    exit 1
+fi
+
+# Input folder?
+INPUT_FOLDER="$1"
+if [ ! -d "$INPUT_FOLDER" ]; then
+    echo "Given location not a folder: $INPUT_FOLDER"
+    exit 2
+fi
+
+
+# Output folder
+SCRIPT_FOLDER="$( cd -- $(dirname "$0"); pwd -P)"
+OUTPUT_FOLDER="$SCRIPT_FOLDER/$OUTPUT_FOLDER"
+if [ ! -d "$OUTPUT_FOLDER" ]; then
+    echo "Check config, output folder not found: $OUTPUT_FOLDER"
+    exit 3
+fi
+
+
+# Create sample files!
+cd $INPUT_FOLDER
+for NAME in *.gz;
+do
+    echo "$NAME"
+    zcat -d "$NAME" | head -n $NUM_ROWS| gzip --fast > "$OUTPUT_FOLDER$NAME"
+done
