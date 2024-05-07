cf77958032df2bc8b185ae4b9d76f4511283c639
Leon Matthews <leon@digitaladvisor.nz>
2024-05-08 11:39:54 +1200

    Simplify and increment version number

--- cine/__init__.py
+++ cine/__init__.py
@@ -1,7 +1,3 @@
 
-__author__ = "Leon Matthews"
-__copyright__ = "Copyright (C) 2019-2022 "+__author__
-__license__ = "GNU General Public License (GPL)"
-__website__ = "http://lost.co.nz/"
-__version_info__ = (0, 3, 0)
+__version_info__ = (0, 4, 0)
 __version__ = '{}.{}.{}'.format(*__version_info__)
